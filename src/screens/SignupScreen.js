c0fff0787c721d7f2085f25af160c52bf5adf63c
sibuor-sudo <otieno1@stanford.edu>
2022-07-08 01:18:39 +0200

    added the functionality to the sign up page

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -16,9 +16,94 @@ const SignupScreen = () => {
     .catch(error =>alert(error.message))
    }
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <KeyboardAvoidingView 
+        style={styles.container}
+        behavior="padding"
+        >
+        <View style={styles.squadLogoContainer}>
+          <Image
+            source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+            style={styles.squadLogo}
+            resizeMode='contain'
+          ></Image>
+        </View>
+
+        <View style={styles.InputContainer}>
+        <TextInput
+            placeholder ="Phone number, email address, or username"
+            value={email}
+            onChangeText={text => setEmail(text)} // everytime a text changes (in our variable it spits out a text variable which we can then use in our function to change the text variable) we can set the email to that text
+            style={styles.input}
+            />
+            <TextInput
+            placeholder ="Password"
+            value={password}
+            onChangeText={text =>setPassword(text)} // everytime a text changes (in our variable it spits out a text variable which we can then use in our function to change the text variable) we can set the password to that text
+            style={styles.input}
+            secureTextEntry
+            />
+        </View> 
+        {/*this view contains our buttons */}
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+            onPress={handleLogin}
+            style = {styles.button}
+                >
+                <Text style={styles.buttonText}>
+                    Login
+                </Text>
+
+            </TouchableOpacity>
+
+            
+                {/* <Text>Item to give cash credit for:</Text>*/}
+                    <View style={[{flexDirection:"row"},styles.signUpContanier]}>
+                        <View style={[{flex:1},styles.textWrapper]}>
+                            <Text style={[{justifyContent: 'flex-start'},styles.text]} >  Don't have an account?</Text>
+                        </View>
+                        <TouchableOpacity  onPress={() =>
+                        navigation.replace('SignupScreen')}
+                        style={{flex:1}}>
+                            <Text  style={[{justifyContent: 'flex-end'},styles.signUpText]}> SignUp </Text>
+                        </TouchableOpacity>
+                        </View>
+                    </View>
+                    {/*this is the straight line with the text in it*/}
+                    <View style={[{flexDirection: 'row'}, styles.horizontalLineContainer]}>
+                        <View style={{backgroundColor: 'black', height: 1, flex: 1, alignSelf: '10', marginLeft:70, flexDirection:"column"}} />
+                        <Text style={{ alignSelf:'10', paddingHorizontal:5, fontSize: 10, marginBottom:50, flexDirection:"column" }}>OR</Text>
+                        <View style={{backgroundColor: 'black', height: 1, flex: 1, alignSelf: '10', marginRight:60}} />
+                    </View>
+
+                    {/**this is the view with the google and the facebook icons */}
+                <View style= {{flexDirection:"row"}}>
+                    <View style= {{flex:1}}>
+                        <Image
+                        source={require('/Users/sheldonotieno/Squad/assets/google-logo.png')}
+                        style= {[{justifyContent:'flex-start'}, styles.googleLogo]}
+                        ></Image>
+                    </View>
+                    <View style= {{fex:1}}>
+                            <Image
+                            source={require('/Users/sheldonotieno/Squad/assets/facebooklogo.png')}
+                            style={[{justifyContent:'flex-end'},styles.facebookLogo]}
+                            >
+                            </Image>
+                    </View>       
+                </View>
+
+                {/**this is the forgot password functionality */}
+                <View style={styles.forgotPasswordTextContainer}>
+                    <Text style={styles.forgotPasswordText}>
+                        Forgot Password?
+                    </Text>
+
+                </View>
+
+
+
+
+        </KeyboardAvoidingView>
   )
 }
 
