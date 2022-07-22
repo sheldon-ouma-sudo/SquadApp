08f983597a219dc48237b925a7f4a992c6f6594a
sibuor-sudo <otieno1@stanford.edu>
2022-07-22 17:42:38 +0200

    styling the emailOTP page

--- src/EmailOTPScreen.js
+++ src/EmailOTPScreen.js
@@ -1,7 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView,StyleSheet, Image, TextInput,TouchableOpacity} from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/core';
 
 const EmailOTPScreen = () => {
+  const navigation = useNavigation()
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -16,7 +18,7 @@ const EmailOTPScreen = () => {
             </View>
           <View>
             <TextInput
-                placeholder ="Enter Confirmtion Code"
+                placeholder ="Enter the confirmation code"
               // value={email}
                 autoCapitalize='none'
                 textAlign = 'center'
@@ -40,15 +42,86 @@ const EmailOTPScreen = () => {
 
           <View style={[{flexDirection:"row"},styles.textContanier]}>
             <View style={[{flex:1},styles.textWrapper]}>
-                <Text style={[{justifyContent: 'flex-start'},styles.confirmationText]} >  Didn't get the code?</Text>
+                <Text style={[{justifyContent: 'flex-start'},styles.confirmationText]} >  Wrong Email?</Text>
             </View>
             <TouchableOpacity  onPress={() =>
-            navigation.replace('EmailOTPScreen')}
+            navigation.replace('SignupScreen')}
             style={{flex:1}}>
-                <Text  style={[{justifyContent: 'flex-end'},styles.confirmationText]}> Try with email </Text>
+                <Text  style={[{justifyContent: 'flex-end'},styles.confirmationText]}> Return to SignUp Page </Text>
             </TouchableOpacity>
           </View>
           </KeyboardAvoidingView>
 )}
 
 export default EmailOTPScreen
+
+const styles = StyleSheet.create({
+  container:{
+  flex:1,
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor: "#F4F8FB"
+  },
+  squadLogo:{
+      width:221,
+      height:85,
+      marginBottom:30
+
+
+  },
+
+  input:{
+    backgroundColor: '#EAEAEA',
+    paddingHorizontal: 15,
+    paddingVertical:10,
+    borderRadius:5,
+    width:296,
+    height:42,
+    marginTop:10,
+    fontSize: 13,
+    marginRight:15,
+    marginLeft:10,
+    fontStyle:"Montserrat",
+    color:'#535353',
+    fontWeight:'400'   
+},
+button:{
+  backgroundColor: '#1145FD',
+  width: 296,
+  height: 42,
+  padding: 15,
+  borderRadius: 5,
+  marginTop: 10,
+  alignItems: 'center',
+  marginRight: 15,
+  marginLeft:10,
+  marginBottom:300
+},
+buttonText:{
+  color: 'white',
+  fontWeight: '700',
+  fontSize: 12.5
+  
+},
+textWrapper:{
+  textAlign:'left',
+  marginTop:5,
+  flexDirection:'row',
+  justifyContent:'flex-start',
+  alignItems:'flex-start'
+
+},
+textContanier:{
+  justifyContent:"center",
+  marginLeft:80,
+  marginRight:20,
+  marginTop:-280,
+  marginBottom:300
+  
+  
+},
+confirmationText:{
+  fontWeight:'500'
+}
+
+})
