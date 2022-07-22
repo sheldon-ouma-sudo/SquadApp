4727b9c91b91832d332398515fb5c692853a5c1e
sibuor-sudo <otieno1@stanford.edu>
2022-07-22 17:21:32 +0200

    styling the email otp

--- src/EmailOTPScreen.js
+++ src/EmailOTPScreen.js
@@ -3,10 +3,52 @@ import React from 'react'
 
 const EmailOTPScreen = () => {
   return (
-    <View>
-      
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      style={styles.container}
+            behavior="padding"
+            >
+            <View style={styles.squadLogoContainer}>
+            <Image
+                source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+                style={styles.squadLogo}
+                resizeMode='contain'
+            ></Image>
+            </View>
+          <View>
+            <TextInput
+                placeholder ="Enter Confirmtion Code"
+              // value={email}
+                autoCapitalize='none'
+                textAlign = 'center'
+                keyboardType="numeric"
+                //onChangeText={text => setEmail(text)} // everytime a text changes (in our variable it spits out a text variable which we can then use in our function to change the text variable) we can set the email to that text
+                style={styles.input}
+                />
+          </View>
+          <View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+            ///onPress={handleSignUp}
+            style = {styles.button}
+                >
+                <Text style={styles.buttonText}>
+                  Confirm
+                </Text>
+              </TouchableOpacity>
+          </View>
+          </View>
+
+          <View style={[{flexDirection:"row"},styles.textContanier]}>
+            <View style={[{flex:1},styles.textWrapper]}>
+                <Text style={[{justifyContent: 'flex-start'},styles.confirmationText]} >  Didn't get the code?</Text>
+            </View>
+            <TouchableOpacity  onPress={() =>
+            navigation.replace('EmailOTPScreen')}
+            style={{flex:1}}>
+                <Text  style={[{justifyContent: 'flex-end'},styles.confirmationText]}> Try with email </Text>
+            </TouchableOpacity>
+          </View>
+          </KeyboardAvoidingView>
+)}
 
 export default EmailOTPScreen
