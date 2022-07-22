391c472e1091ad22f930e9972d1ebe182339241f
sibuor-sudo <otieno1@stanford.edu>
2022-07-22 14:17:09 +0200

    styling the phoneOTP page

--- src/screens/PhoneOTPScreen.js
+++ src/screens/PhoneOTPScreen.js
@@ -1,123 +1,123 @@
-import { View, Text, KeyboardAvoidingView,StyleSheet, Image, TextInput,TouchableOpacity} from 'react-native'
-import React from 'react'
+  import { View, Text, KeyboardAvoidingView,StyleSheet, Image, TextInput,TouchableOpacity} from 'react-native'
+  import React from 'react'
 
-const PhoneOTPScreen = () => {
-  return (
-    <KeyboardAvoidingView
-      style={styles.container}
-            behavior="padding"
-            >
-            <View style={styles.squadLogoContainer}>
-            <Image
-                source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
-                style={styles.squadLogo}
-                resizeMode='contain'
-            ></Image>
+  const PhoneOTPScreen = () => {
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+              behavior="padding"
+              >
+              <View style={styles.squadLogoContainer}>
+              <Image
+                  source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+                  style={styles.squadLogo}
+                  resizeMode='contain'
+              ></Image>
+              </View>
+            <View>
+              <TextInput
+                  placeholder ="Enter Confirmtion Code"
+                // value={email}
+                  autoCapitalize='none'
+                  textAlign = 'center'
+                  keyboardType="numeric"
+                  //onChangeText={text => setEmail(text)} // everytime a text changes (in our variable it spits out a text variable which we can then use in our function to change the text variable) we can set the email to that text
+                  style={styles.input}
+                  />
             </View>
-          <View>
-            <TextInput
-                placeholder ="Enter Confirmtion Code"
-               // value={email}
-                autoCapitalize='none'
-                textAlign = 'center'
-                keyboardType="numeric"
-                //onChangeText={text => setEmail(text)} // everytime a text changes (in our variable it spits out a text variable which we can then use in our function to change the text variable) we can set the email to that text
-                style={styles.input}
-                />
-          </View>
-          <View>
-           <View style={styles.buttonContainer}>
-            <TouchableOpacity
-            ///onPress={handleSignUp}
-            style = {styles.button}
-                >
-                <Text style={styles.buttonText}>
-                  Confirm
-                </Text>
+            <View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+              ///onPress={handleSignUp}
+              style = {styles.button}
+                  >
+                  <Text style={styles.buttonText}>
+                    Confirm
+                  </Text>
+                </TouchableOpacity>
+            </View>
+            </View>
+
+            <View style={[{flexDirection:"row"},styles.textContanier]}>
+              <View style={[{flex:1},styles.textWrapper]}>
+                  <Text style={[{justifyContent: 'flex-start'},styles.text]} >  Didn't get the code?</Text>
+              </View>
+              <TouchableOpacity  onPress={() =>
+              navigation.replace('SignupScreen')}
+              style={{flex:1}}>
+                  <Text  style={[{justifyContent: 'flex-end'},styles.signUpText]}> Try with email </Text>
               </TouchableOpacity>
-           </View>
-          </View>
-
-          <View style={[{flexDirection:"row"},styles.textContanier]}>
-            <View style={[{flex:1},styles.textWrapper]}>
-                <Text style={[{justifyContent: 'flex-start'},styles.text]} >  Didn't get the code?</Text>
             </View>
-            <TouchableOpacity  onPress={() =>
-            navigation.replace('SignupScreen')}
-            style={{flex:1}}>
-                <Text  style={[{justifyContent: 'flex-end'},styles.signUpText]}> Try with email </Text>
-            </TouchableOpacity>
-           </View>
-    </KeyboardAvoidingView>
-  )
-}
+      </KeyboardAvoidingView>
+    )
+  }
 
-export default PhoneOTPScreen
+  export default PhoneOTPScreen
 
 
-const styles = StyleSheet.create({
-  container:{
-  flex:1,
-  justifyContent:"center",
-  alignItems:"center",
-  backgroundColor: "#F4F8FB"
+  const styles = StyleSheet.create({
+    container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor: "#F4F8FB"
+    },
+    squadLogo:{
+        width:221,
+        height:85,
+        marginBottom:30
+
+
+    },
+
+    input:{
+      backgroundColor: '#EAEAEA',
+      paddingHorizontal: 15,
+      paddingVertical:10,
+      borderRadius:5,
+      width:296,
+      height:42,
+      marginTop:10,
+      fontSize: 13,
+      marginRight:15,
+      marginLeft:10,
+      fontStyle:"Montserrat",
+      color:'#535353',
+      fontWeight:'400'   
   },
-  squadLogo:{
-      width:221,
-      height:85,
-      marginBottom:30
-
+  button:{
+    backgroundColor: '#1145FD',
+    width: 296,
+    height: 42,
+    padding: 15,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft:15,
+    marginBottom:300
+  },
+  buttonText:{
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 12.5
+    
+  },
+  textWrapper:{
+    textAlign:'left',
+    marginTop:5,
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems:'flex-start'
 
   },
+  textContanier:{
+    justifyContent:"center",
+    marginLeft:80,
+    marginTop:-280,
+    marginBottom:300
+    
+    
+  }
 
-  input:{
-    backgroundColor: '#EAEAEA',
-    paddingHorizontal: 15,
-    paddingVertical:10,
-    borderRadius:5,
-    width:296,
-    height:42,
-    marginTop:10,
-    fontSize: 13,
-    marginRight:15,
-    marginLeft:10,
-    fontStyle:"Montserrat",
-    color:'#535353',
-    fontWeight:'400'   
-},
-button:{
-  backgroundColor: '#1145FD',
-  width: 296,
-  height: 42,
-  padding: 15,
-  borderRadius: 5,
-  marginTop: 10,
-  alignItems: 'center',
-  marginRight: 10,
-  marginLeft:15,
-  marginBottom:300
-},
-buttonText:{
-  color: 'white',
-  fontWeight: '700',
-  fontSize: 12.5
-  
-},
-textWrapper:{
-  textAlign:'left',
-  marginTop:5,
-  flexDirection:'row',
-  justifyContent:'flex-start',
-  alignItems:'flex-start'
-
-},
-textContanier:{
-  justifyContent:"center",
-  marginLeft:80,
-  marginTop:-280,
-  marginBottom:300
-  
-  
-}
-
-})
+  })
