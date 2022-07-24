0684082d2248dc22a7e0a62fec4316d9591c171d
sibuor-sudo <otieno1@stanford.edu>
2022-07-24 13:39:22 +0200

    all untracked changes

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -168,37 +168,39 @@ auth
 }
 
 }
+
 //function that handles the phone number part of the app
-const phoneFormat = (value) => {
+function formatPhoneNumber(value) {
     // if input value is falsy eg if the user deletes the input, then just return
-   if (!value) return value;
-
-  // clean the input for any non-digit values.
-  const number = value.replace(/[^\d]/g, '');
-
-  // phoneNumberLength is used to know when to apply our formatting for the phone number
-  const phoneNumberLength = number.length;
-
-  // we need to return the value with no formatting if its less then four digits
-  // this is to avoid weird behavior that occurs if you  format the area code to early
-  if (phoneNumberLength < 4) return number;
-
-
-  // if phoneNumberLength is greater than 4 and less the 7 we start to return
-  // the formatted number
-  if (phoneNumberLength < 7) {
-    return `(${number.slice(0, 3)}) ${number.slice(3)}`;
-  }// var match = number.match(/(\d{3})(\d{3})(\d{4})$/)
-    if (number>7) {
-        let num = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3,6 )}-${phoneNumber.slice(6, 10)}`;
-        //['(', match[1], ') ', match[2], '-', match[3]].join('');
-        number= callingCode + num;
-        setPhone(number);
-        return;
+    if (!value) return value;
+  
+    // clean the input for any non-digit values.
+    const phoneNum = value.replace(/[^\d]/g, '');
+  
+    // phoneNumberLength is used to know when to apply our formatting for the phone number
+    const phoneNumberLength = phoneNum.length;
+  
+    // we need to return the value with no formatting if its less then four digits
+    // this is to avoid weird behavior that occurs if you  format the area code to early
+  
+    if (phoneNumberLength < 4) return phoneNum;
+  
+    // if phoneNumberLength is greater than 4 and less the 7 we start to return
+    // the formatted number
+    if (phoneNumberLength < 7) {
+      return `(${phoneNum.slice(0, 3)}) ${phoneNum.slice(3)}`;
     }
-    
-    setPhone(number);
-    }
+  
+    // finally, if the phoneNumberLength is greater then seven, we add the last
+    // bit of formatting and return it.
+    return `(${phoneNum.slice(0, 3)}) ${phoneNum.slice(3, 6 )}-${phoneNumber.slice(6, 10)}`;
+  }
+  const handleInput = (e) => {
+    // this is where we'll call the phoneNumberFormatter function
+    const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+    // we'll set the input value using our setInputValue
+    setPhone(formattedPhoneNumber);
+  };
     
 return (
     <KeyboardAvoidingView 
@@ -260,10 +262,10 @@ return (
                 <TextInput       
                     maxLength={10}
                     keyboardType="number-pad"
-                    onChangeText={number => phoneFormat(number)}
+                    onChange={(e) => handleInput(e)} value={phoneNumber} 
                     placeholder="Enter Phone Number"
                     // textAlign = 'center'
-                    value={phoneNumber}
+                   // value={phoneNumber}
                     style={[{justifyContent:'flex-end'}]}
                     />
             </View>
