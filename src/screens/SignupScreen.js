b15e71e2ba36bce6151e9bd3b1626be52c7aebb8
sibuor-sudo <otieno1@stanford.edu>
2022-07-24 13:19:11 +0200

    formating the phone number

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -169,13 +169,34 @@ auth
 
 }
 //function that handles the phone number part of the app
-const phoneFormat = (number) => {
-    var match = number.match(/(\d{3})(\d{3})(\d{4})$/)
-    if (match) {
-        let num = ['(', match[1], ') ', match[2], '-', match[3]].join('');
-        setPhone(num);
+const phoneFormat = (value) => {
+    // if input value is falsy eg if the user deletes the input, then just return
+   if (!value) return value;
+
+  // clean the input for any non-digit values.
+  const number = value.replace(/[^\d]/g, '');
+
+  // phoneNumberLength is used to know when to apply our formatting for the phone number
+  const phoneNumberLength = number.length;
+
+  // we need to return the value with no formatting if its less then four digits
+  // this is to avoid weird behavior that occurs if you  format the area code to early
+  if (phoneNumberLength < 4) return number;
+
+
+  // if phoneNumberLength is greater than 4 and less the 7 we start to return
+  // the formatted number
+  if (phoneNumberLength < 7) {
+    return `(${number.slice(0, 3)}) ${number.slice(3)}`;
+  }// var match = number.match(/(\d{3})(\d{3})(\d{4})$/)
+    if (number>7) {
+        let num = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3,6 )}-${phoneNumber.slice(6, 10)}`;
+        //['(', match[1], ') ', match[2], '-', match[3]].join('');
+        number= callingCode + num;
+        setPhone(number);
         return;
     }
+    
     setPhone(number);
     }
     
