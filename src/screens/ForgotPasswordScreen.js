ffd5f2b6a0f212022da105025e4d8c823689bbf3
sibuor-sudo <otieno1@stanford.edu>
2022-07-25 13:57:00 +0200

    all untracked changes

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -1,261 +1,130 @@
-import { View, Text,KeyboardAvoidingView,Image,StyleSheet,TextInput} from 'react-native'
-import React from 'react'
-import { CountryCode } from 'react-native-country-picker-modal'
-import CountryPicker from 'react-native-country-picker-modal'
-import { useState } from 'react'
+import * as React from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import { FirebaseRecaptchaVerifierModal, FirebaseRecaptchaBanner } from 'expo-firebase-recaptcha';
+import { initializeApp, getApp } from 'firebase/app';
+import { getAuth, PhoneAuthProvider, signInWithCredential } from 'firebase/auth';
 
+// Initialize Firebase JS SDK >=9.x.x
+// https://firebase.google.com/docs/web/setup
+/*try {
+  initializeApp({
+    ...
+  });
+} catch (err) {
+  // ignore app already initialized error in snack
+}*/
 
+// Firebase references
+const app = getApp();
+const auth = getAuth();
 
-const ForgotPasswordScreen = () => {
-const[countryCode, setCountryCode] = useState('US')
-const[callingCode, setCallingCode] = useState('1')
-const [phoneNumber, setPhone] = useState('')
-//function that handles the phone number part of the app
-const phoneFormat = (number) => {
-    var match = number.match(/(\d{3})(\d{3})(\d{4})$/)
-    if (match) {
-        let num = ['(', match[1], ') ', match[2], '-', match[3]].join('');
-        setPhone(num);
-        return;
-    }
-    setPhone(number);
-    }
-    
-
-  return ( 
-    <KeyboardAvoidingView
-    
-    style={styles.container}
-    behavior="padding"
-    >
-    <View style={styles.squadLogoContainer}>
-    <Image
-        source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
-        style={styles.squadLogo}
-        resizeMode='contain'
-    ></Image>
-    </View>
-    <View style={{flexDirection:'row'}}>
-        <View style={[{flex:1},styles.countryCodeInput]}>
-         <CountryPicker
-            withFilter
-            withAlphaFilter={false}
-            withCurrencyButton={false}
-            withCallingCode
-            withFlag
-            countryCode={countryCode}
-            onSelect={country =>{
-                const{cca2, callingCode} = country
-                setCountryCode(cca2)
-                setCallingCode(callingCode[0])
-            }}
-            containerButtonStyle={{
-            /// alignItems:'center',
-            //justifyContent:'flex-start',
-                marginLeft:50  
-            }}
-            style={[{justifyContent:'flex-start'},styles.countryCodeInput]}
-        />
-     </View>
-     <View style={[{flex:1},styles.phoneNumberInput]}>
-        <TextInput       
-            maxLength={10}
-            keyboardType="number-pad"
-            onChangeText={number => phoneFormat(number)}
-            placeholder="Enter Phone Number"
-            // textAlign = 'center'
-            value={phoneNumber}
-            style={[{justifyContent:'flex-end'}]}
-            />
-     </View>
-    </View>
-    
-        
-        
-    </KeyboardAvoidingView>
-  )
+// Double-check that we can run the example
+if (!app?.options || Platform.OS === 'web') {
+  throw new Error('This example only works on Android or iOS, and requires a valid Firebase config.');
 }
 
+export default function App() {
+  // Ref or state management hooks
+  const recaptchaVerifier = React.useRef(null);
+  const [phoneNumber, setPhoneNumber] = React.useState();
+  const [verificationId, setVerificationId] = React.useState();
+  const [verificationCode, setVerificationCode] = React.useState();
 
-const styles = StyleSheet.create({
-    container:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor: "#F4F8FB"
-    },
-    squadLogo:{
-        width:221,
-        height:85
+  const firebaseConfig = app ? app.options : undefined;
+  const [message, showMessage] = React.useState();
+  const attemptInvisibleVerification = false;
 
-
-    },
-    InputContainer:{
-        width: 296,
-        right: 16,
-        marginLeft:29,
-        marginTop: 20,
-        borderRadius:5
-
-    },
-
-    input:{
-        backgroundColor: '#EAEAEA',
-        paddingHorizontal: 15,
-        paddingVertical:10,
-        borderRadius:5,
-        width:296,
-        height:42,
-        marginTop:10,
-        fontSize: 13,
-        marginRight:15,
-        marginLeft:10,
-        fontStyle:"Montserrat",
-        color:'#535353',
-        fontWeight:'400'   
-    },
-    phoneNumberInput:{
-        backgroundColor: '#EAEAEA',
-        paddingHorizontal: 25,
-        paddingVertical:10,
-        borderRadius:5,
-        width:280,
-        height:42,
-        marginTop:10,
-        fontSize: 13,
-        marginRight:35,
-        marginLeft:10,
-        fontStyle:"Montserrat",
-        color:'#535353',
-        fontWeight:'400'   
-    },
-    countryCodeInput:{
-        backgroundColor: '#EAEAEA',
-        paddingHorizontal: 5,
-        paddingVertical:10,
-        borderRadius:5,
-        width:16,
-        height:42,
-        marginTop:10,
-        fontSize: 13,
-        marginRight:3,
-        marginLeft:50,
-        fontStyle:"Montserrat",
-        color:'#535353',
-        fontWeight:'400'   
-    },
-    errorText:{
-        color:'#FFFFF',
-        marginLeft:20,
-        fontSize:12,
-        //textAlign:'center'
-
-
-    },
-        buttonContainer:{
-        width: 296,
-        height:42,
-        borderRadius:5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 23,
-        marginBottom: 60
-    },
-
-    button:{
-        backgroundColor: '#1145FD',
-        width: 296,
-        height: 42,
-        padding: 15,
-        borderRadius: 5,
-        marginTop: -2,
-        alignItems: 'center',
-        marginRight: 10,
-        marginLeft:15,
-        top: 0
-
-
-    },
-    buttonText:{
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 12.5
-        
-    },
-    buttonOutline:{
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#1145FD',
-    borderWidth: 2,
-
-
-    },
-    buttonOutlineText:{
-        color: '#1145FD',
-        fontWeight: '700',
-        fontSize: 16
-    },
-    textWrapper:{
-        textAlign:'left',
-        marginTop:5,
-        flexDirection:'row',
-        justifyContent:'flex-start',
-        alignItems:'flex-start'
-
-    },
-
-    text:{
-    fontSize: 12,
-    textAlign: 'left',
-    marginTop:10,
-    marginBottom: 10,
-    marginLeft:10,
-    alignItems:'left',
-    fontStyle: "Montserrat"
-
-
-    },
-    loginText:{
-        fontSize: 14,
-        marginTop: -40,
-        marginBottom:10,
-        marginLeft: 10,
-        fontWeight:'bold',
-        fontStyle:"Montserrat"
-    },
-    horizontalLineContainer:{},
-    googleLogo:{
-    height: 30, 
-    width:30,
-    borderRadius:20,
-    overflow:'hidden',
-    borderWidth:1,
-    borderColor: "red",
-    marginLeft:180,
-    marginTop:-5
-
-    },
-    facebookLogo:{
-        height: 30, 
-        width:30,
-        borderRadius:20,
-        overflow:'hidden',
-        borderWidth:1,
-        borderColor: "red",
-        marginRight:170,
-        marginTop:-5
-    },
-    logo:{
-    marginTop:-37
-    },
-    phoneContainer: {
-        width: '75%',
-        height: 50,
-        marginLeft: 10
-    },
-    textInput: {
-        paddingVertical: 0,
-    }
-    })
-
-export default ForgotPasswordScreen
+  return (
+    <View style={{ padding: 20, marginTop: 50 }}>
+      <FirebaseRecaptchaVerifierModal
+        ref={recaptchaVerifier}
+        firebaseConfig={app.options}
+        // attemptInvisibleVerification
+      />
+      <Text style={{ marginTop: 20 }}>Enter phone number</Text>
+      <TextInput
+        style={{ marginVertical: 10, fontSize: 17 }}
+        placeholder="+1 999 999 9999"
+        autoFocus
+        autoCompleteType="tel"
+        keyboardType="phone-pad"
+        textContentType="telephoneNumber"
+        onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
+      />
+      <Button
+        title="Send Verification Code"
+        disabled={!phoneNumber}
+        onPress={async () => {
+          // The FirebaseRecaptchaVerifierModal ref implements the
+          // FirebaseAuthApplicationVerifier interface and can be
+          // passed directly to `verifyPhoneNumber`.
+          try {
+            const phoneProvider = new PhoneAuthProvider(auth);
+            const verificationId = await phoneProvider.verifyPhoneNumber(
+              phoneNumber,
+              recaptchaVerifier.current
+            );
+            setVerificationId(verificationId);
+            showMessage({
+              text: 'Verification code has been sent to your phone.',
+            });
+          } catch (err) {
+            showMessage({ text: `Error: ${err.message}`, color: 'red' });
+          }
+        }}
+      />
+      <Text style={{ marginTop: 20 }}>Enter Verification code</Text>
+      <TextInput
+        style={{ marginVertical: 10, fontSize: 17 }}
+        editable={!!verificationId}
+        placeholder="123456"
+        onChangeText={setVerificationCode}
+      />
+      <Button
+        title="Confirm Verification Code"
+        disabled={!verificationId}
+        onPress={async () => {
+          try {
+            const credential = PhoneAuthProvider.credential(
+              verificationId,
+              verificationCode
+            );
+            await signInWithCredential(auth, credential);
+            showMessage({ text: 'Phone authentication successful 👍' });
+          } catch (err) {
+            showMessage({ text: `Error: ${err.message}`, color: 'red' });
+          }
+        }}
+      />
+      {message ? (
+        <TouchableOpacity
+          style={[
+            StyleSheet.absoluteFill,
+            { backgroundColor: 0xffffffee, justifyContent: 'center' },
+          ]}
+          onPress={() => showMessage(undefined)}>
+          <Text
+            style={{
+              color: message.color || 'blue',
+              fontSize: 17,
+              textAlign: 'center',
+              margin: 20,
+            }}>
+            {message.text}
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        undefined
+      )}
+      {attemptInvisibleVerification && <FirebaseRecaptchaBanner />}
+    </View>
+  );
+}
