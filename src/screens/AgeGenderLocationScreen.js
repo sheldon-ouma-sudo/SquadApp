4377727a0fd036ae4cf3e60798f488a38ebd8ffe
sibuor-sudo <otieno1@stanford.edu>
2022-07-29 12:15:33 +0200

    all untracked changes

---
+++ src/screens/AgeGenderLocationScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AgeGenderLocationScreen = () => {
+  return (
+    <View>
+      <Text>AgeGenderLocationScreen</Text>
+    </View>
+  )
+}
+
+export default AgeGenderLocationScreen
---
+++ src/screens/ProfilePictureUpload.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfilePictureUpload = () => {
+  return (
+    <View>
+      <Text>ProfilePictureUpload</Text>
+    </View>
+  )
+}
+
+export default ProfilePictureUpload
