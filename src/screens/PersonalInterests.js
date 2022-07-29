179e4588c6151d7617abc32daca1b22540a4df55
sibuor-sudo <otieno1@stanford.edu>
2022-07-29 12:18:45 +0200

    added the screens

---
+++ src/screens/PersonalInterests.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PersonalInterests = () => {
+  return (
+    <View>
+      <Text>PersonalInterests</Text>
+    </View>
+  )
+}
+
+export default PersonalInterests
