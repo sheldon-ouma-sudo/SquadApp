e2a1441fd7f24a847397bc8a26713f207fcf6af4
sibuor-sudo <otieno1@stanford.edu>
2022-07-31 22:56:34 +0200

    adding the material UI dependencies into the project

--- src/screens/AgeGenderLocationScreen.js
+++ src/screens/AgeGenderLocationScreen.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text,KeyboardAvoidingView,Image, StyleSheet} from 'react-native'
 import React from 'react'
-
 const AgeGenderLocationScreen = () => {
   return (
-    <View>
-      <Text>AgeGenderLocationScreen</Text>
-    </View>
+    <KeyboardAvoidingView 
+        style={styles.container}
+        behavior="padding"
+        >
+        <View style={styles.squadLogoContainer}>
+          <Image
+            source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+            style={styles.squadLogo}
+            resizeMode='contain'
+          ></Image>
+        </View>      
+
+        
+
+        </KeyboardAvoidingView>
   )
 }
 
