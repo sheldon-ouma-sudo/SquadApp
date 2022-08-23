3e614d683455e372905c50f059475864a37613e5
sibuor-sudo <otieno1@stanford.edu>
2022-08-23 14:51:48 +0200

    creating the UI for the phone input

--- src/screens/PhoneNumberScreen.js
+++ src/screens/PhoneNumberScreen.js
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView, StyleSheet } from 'react-native'
 import React from 'react'
 
 const PhoneNumberScreen = () => {
   return (
-    <View>
-      <Text>PhoneNumberScreen</Text>
-    </View>
+      <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding"
+    >
+        <View style={styles.squadLogoContainer}>
+            <Image
+                source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+                style={styles.squadLogo}
+                resizeMode='contain'
+            ></Image>
+            </View>
+      </KeyboardAvoidingView>
+    
   )
 }
 
+
+const styles = StyleSheet.create({
+    container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor: "#F4F8FB"
+    },
+    squadLogo:{
+        width:221,
+        height:85,
+        marginBottom:30
+    },
+})
 export default PhoneNumberScreen
