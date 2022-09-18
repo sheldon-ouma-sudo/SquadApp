75b6d693b2745faf42676fcfd5168281b5ba47eb
sibuor-sudo <otieno1@stanford.edu>
2022-09-18 18:10:27 +0300

    adding more screens

---
+++ src/screens/PersonalPollScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PersonalPollScreen = () => {
+  return (
+    <View>
+      <Text>PersonalPollScreen</Text>
+    </View>
+  )
+}
+
+export default PersonalPollScreen
---
+++ src/screens/SquadScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SquadScreen = () => {
+  return (
+    <View>
+      <Text>SquadScreen</Text>
+    </View>
+  )
+}
+
+export default SquadScreen
