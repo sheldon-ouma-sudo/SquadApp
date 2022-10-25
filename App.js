0bfc7ef18aed303d79fc09d0be61273b967997d0
sibuor-sudo <otieno1@stanford.edu>
2022-10-25 15:32:04 -0700

    adding all the changes on the previous, i.e fixing the top tab navigation

--- App.js
+++ App.js
@@ -37,78 +37,7 @@
         
       //this is the create stack navigator method: src from the navigation docs on the internet
       const Stack = createNativeStackNavigator();
-      LogBox.ignoreLogs(["EventEmitter.removeListener"]);
-      const TopHomeTabNavigator = createMaterialTopTabNavigator({
-        TrendingScreen: {
-          screen: TrendingPollScreen, 
-          navigationOptions: {
-          // tabBarLabel: "Home",
-            tabBarOptions: {
-              activeTintColor: '#1145FD',
-              title: 'Trending'
-            },
-            
-          },
-        },
-        PublicPollScreen: {
-          screen: PublicPollSquadScreen,
-          navigationOptions: {
-            tabBarLabel: "Public Polls",
-            tabBarOptions: {
-              activeTintColor: '#1145FD',
-            },
-          },
-        },
-        SquadPollScreen: {
-          screen: MySquadPollScreen,
-          navigationOptions: {
-            tabBarLabel: "Squad Polls",
-            tabBarOptions: {
-              activeTintColor: '#1145FD',
-            },
-            
-          },
-        },          
-      })
-
-
-
-      const TopProflileTabNavigator = createMaterialTopTabNavigator({
-        MySquadScreen: {
-          screen: MySquadScreen, 
-          navigationOptions: {
-            tabBarLabel: "Squad",
-            tabBarOptions: {
-              activeTintColor: '#1145FD',
-            },
-            
-          },
-        },
-        Polls: {
-          screen: PersonalPollScreen,
-          navigationOptions: {
-            tabBarLabel: "Polls",
-            tabBarOptions: {
-              activeTintColor: '#1145FD',
-            },
-            
-          },
-        },
-        Squad: {
-          screen: SquadScreen,
-          navigationOptions: {
-            tabBarLabel: "Squads",
-            tabBarOptions: {
-              activeTintColor: '#1145FD',
-            },
-            
-          },
-        },
-      
-      })
-
-
-
+      LogBox.ignoreLogs(["EventEmitter.removeListener"]);  
 
       const TabNavigator = createBottomTabNavigator({
         Home: {
@@ -213,18 +142,10 @@
 
     )
   }
-  const HomeTopTabNavigator = createAppContainer(TopHomeTabNavigator)
-  const ProfileTopTabNavigator = createAppContainer(TopProflileTabNavigator)
-  function HomeScreenTopTabNavigator(){
-        <HomeTopTabNavigator>
-            <HomeScreen/>
-        </HomeTopTabNavigator>
-  }
-  function ProfileScreenTopTabNavigator(){
-  <ProfileTopTabNavigator>
-    <Profile/>
-  </ProfileTopTabNavigator>
-  }
+  //const HomeTopTabNavigator = createAppContainer(TopHomeTabNavigator)
+  ///const ProfileTopTabNavigator = createAppContainer(TopProflileTabNavigator)
+  
+  //function ProfileScreenTopTabNavigator(){<ProfileTopTabNavigator><Profile/></ProfileTopTabNavigator> }
       export default function App() {
         return (
           <NavigationContainer>
@@ -232,8 +153,6 @@
             {/*<Stack.Screen name="Home"component={HomeScreen}options={{ title: 'Welcome' }}/>*/}
               <Stack.Screen options={{headerShown : false}}   name="LoginScreen" component={LoginScreen} />
               <Stack.Screen name="HomeScreenBottomNavigator" options={{headerShown: false}} component={HomeScreenBottonNavigator} />
-              <Stack.Screen name= "HomeScreenTopNavigator"    options={{headerShown:false}} component={HomeScreenTopTabNavigator}/>
-              <Stack.Screen  name = "ProfileScreenTopTabNavigator" options={{headerShow:false}} component={ProfileScreenTopTabNavigator}/>
               <Stack.Screen options={{headerShown: false}} name="SignupScreen"  component={SignupScreen} />
                <Stack.Screen options={{headerShown:false}} name="PasswordResetScreen" component={PasswordResetScreen}/>
               <Stack.Screen options={{headerShown: false}} name="SquadCreationScreen"  component={SquadCreationScreen} />
