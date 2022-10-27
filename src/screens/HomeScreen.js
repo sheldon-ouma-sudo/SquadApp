a4963e3602ef1c9f1af81e4aa9a527eb595130b6
sibuor-sudo <otieno1@stanford.edu>
2022-10-26 20:43:23 -0700

    adding all the changes so far

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,10 @@
-import { View, Text,KeyboardAvoidingView,Image, StyleSheet, 
-  StatusBar,Dimensions,TouchableOpacity} from 'react-native'
+import { View, Text,KeyboardAvoidingView,Image, StyleSheet, StatusBar,Dimensions,TouchableOpacity} from 'react-native'
 import React from 'react'
+import PublicPollSquadScreen from './PublicPollSquadScreen'
+import TrendingPollScreen from './TrendingPollScreen'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = () => {
   return (
