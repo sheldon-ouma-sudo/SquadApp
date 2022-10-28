1666bdd7f2e11465db62ebc049dcf0908cd1b926
sibuor-sudo <otieno1@stanford.edu>
2022-10-28 14:33:48 -0700

    adding all the screens require

---
+++ src/screens/MediaPreviewScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MediaPreviewScreen = () => {
+  return (
+    <View>
+      <Text>MediaPreviewScreen</Text>
+    </View>
+  )
+}
+
+export default MediaPreviewScreen
