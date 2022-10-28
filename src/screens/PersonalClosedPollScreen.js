9572a28c8e91ae0394e26f37185816dabfa8019c
sibuor-sudo <otieno1@stanford.edu>
2022-10-28 14:27:36 -0700

    adding two more screens for the profile functionality

---
+++ src/screens/PersonalClosedPollScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PersonalClosedPollScreen = () => {
+  return (
+    <View>
+      <Text>PersonalClosedPollScreen</Text>
+    </View>
+  )
+}
+
+export default PersonalClosedPollScreen
---
+++ src/screens/PersonalOpenPOlls.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PersonalOpenPOlls = () => {
+  return (
+    <View>
+      <Text>PersonalOpenPOlls</Text>
+    </View>
+  )
+}
+
+export default PersonalOpenPOlls
