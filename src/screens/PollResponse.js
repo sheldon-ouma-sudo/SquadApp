04d03fb7070acdf2ea0682e0125d025a03456115
sibuor-sudo <otieno1@stanford.edu>
2022-10-28 14:23:18 -0700

    adding new screens, thatis the activity screen, poll response screen and swaying screens

---
+++ src/screens/PollResponse.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PollResponse = () => {
+  return (
+    <View>
+      <Text>PollResponse</Text>
+    </View>
+  )
+}
+
+export default PollResponse
