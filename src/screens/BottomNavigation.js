e17e3db151324ffc1108dc1818ea4fb9c252c99b
sibuor-sudo <otieno1@stanford.edu>
2022-11-02 17:43:42 -0700

    changes on the bottom navigation screen

--- src/screens/BottomNavigation.js
+++ src/screens/BottomNavigation.js
@@ -1,21 +1,136 @@
 import { View, Text,KeyboardAvoidingView,Image, StyleSheet, 
   StatusBar,Dimensions,TouchableOpacity} from 'react-native'
 import React from 'react'
+import NotificationScreen from './NotificationTopNavigation';
+import HomeScreen from './HomeTapNavigation';
+import Profile from './ProfileTopNavigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+
+const Tab = createBottomTabNavigator({
+  Home: {
+      screen: HomeScreen, 
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarOptions: {
+          activeTintColor: '#1145FD',
+        },
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="home"
+              size={24}
+              color={tabInfo.focused ?'#1145FD': "#8e8e93"}
+            />
+          );
+        },
+      },
+    },
+Explore: {
+  screen: ExploreScreen,
+  navigationOptions: {
+    tabBarLabel: "Explore",
+    tabBarOptions: {
+      activeTintColor: '#1145FD',
+    },
+    tabBarIcon: (tabInfo) => {
+      return (
+        <Ionicons
+          name="globe"
+          size={24}
+          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+        />
+      );
+    },
+  },
+},
+PollCreation: {
+  screen: PollCreation,
+  navigationOptions: {
+    tabBarLabel: "",
+    tabBarOptions: {
+      activeTintColor: '#1145FD',
+    },
+    tabBarIcon: (tabInfo) => {
+      return (
+        <Ionicons
+          name="duplicate"
+          size={24}
+          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+        />
+      );
+    },
+  },
+},
+Notification: {
+  screen: NotificationScreen,
+  navigationOptions: {
+    tabBarLabel: "Notifications",
+    tabBarOptions: {
+      activeTintColor: '#1145FD',
+    },
+    tabBarIcon: (tabInfo) => {
+      return (
+        <Ionicons
+          name="notifications"
+          size={24}
+          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+        />
+      );
+    },
+  },
+},
+
+Profile: {
+  screen: Profile,
+  navigationOptions: {
+    tabBarLabel: "Profile",
+    tabBarOptions: {
+      activeTintColor: '#1145FD',
+    },
+    tabBarIcon: (tabInfo) => {
+      return (
+        <Ionicons
+          name="person-circle"
+          size={24}
+          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+        />
+      );
+    },
+  },
+},
+});
+
+
+
+
+
+
+
+
 
 const BottomNavigation = () => {
   return (
-    <KeyboardAvoidingView 
-    style={styles.container}
-    behavior="padding"
+    <><KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
     >
-    <View style={[styles.squadLogoContainer, {flexDirection:'column'}]}>
-      <Image
-        source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
-        style={styles.squadLogo}
-        resizeMode='contain'
-      ></Image>
-    </View>     
+      <View style={[styles.squadLogoContainer, { flexDirection: 'column' }]}>
+        <Image
+          source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+          style={styles.squadLogo}
+          resizeMode='contain'
+        ></Image>
+      </View>
     </KeyboardAvoidingView>
+    <Tab.Navigator>
+      
+      
+      
+      
+      
+      </Tab.Navigator></>
   )
 }
 
