1b44b21de0ebdc6676499b53a0d83d2bd81fda8b
sibuor-sudo <otieno1@stanford.edu>
2022-11-02 18:01:56 -0700

    adding the changes for the bottom navigation home tab

--- src/screens/BottomNavigation.js
+++ src/screens/BottomNavigation.js
@@ -8,99 +8,99 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 
 
-const Tab = createBottomTabNavigator({
-  Home: {
-      screen: HomeScreen, 
-      navigationOptions: {
-        tabBarLabel: "Home",
-        tabBarOptions: {
-          activeTintColor: '#1145FD',
-        },
-        tabBarIcon: (tabInfo) => {
-          return (
-            <Ionicons
-              name="home"
-              size={24}
-              color={tabInfo.focused ?'#1145FD': "#8e8e93"}
-            />
-          );
-        },
-      },
-    },
-Explore: {
-  screen: ExploreScreen,
-  navigationOptions: {
-    tabBarLabel: "Explore",
-    tabBarOptions: {
-      activeTintColor: '#1145FD',
-    },
-    tabBarIcon: (tabInfo) => {
-      return (
-        <Ionicons
-          name="globe"
-          size={24}
-          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
-        />
-      );
-    },
-  },
-},
-PollCreation: {
-  screen: PollCreation,
-  navigationOptions: {
-    tabBarLabel: "",
-    tabBarOptions: {
-      activeTintColor: '#1145FD',
-    },
-    tabBarIcon: (tabInfo) => {
-      return (
-        <Ionicons
-          name="duplicate"
-          size={24}
-          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
-        />
-      );
-    },
-  },
-},
-Notification: {
-  screen: NotificationScreen,
-  navigationOptions: {
-    tabBarLabel: "Notifications",
-    tabBarOptions: {
-      activeTintColor: '#1145FD',
-    },
-    tabBarIcon: (tabInfo) => {
-      return (
-        <Ionicons
-          name="notifications"
-          size={24}
-          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
-        />
-      );
-    },
-  },
-},
+const Tab = createBottomTabNavigator();
+//   Home: {
+//       screen: HomeScreen, 
+//       navigationOptions: {
+//         tabBarLabel: "Home",
+//         tabBarOptions: {
+//           activeTintColor: '#1145FD',
+//         },
+//         tabBarIcon: (tabInfo) => {
+//           return (
+//             <Ionicons
+//               name="home"
+//               size={24}
+//               color={tabInfo.focused ?'#1145FD': "#8e8e93"}
+//             />
+//           );
+//         },
+//       },
+//     },
+// Explore: {
+//   screen: ExploreScreen,
+//   navigationOptions: {
+//     tabBarLabel: "Explore",
+//     tabBarOptions: {
+//       activeTintColor: '#1145FD',
+//     },
+//     tabBarIcon: (tabInfo) => {
+//       return (
+//         <Ionicons
+//           name="globe"
+//           size={24}
+//           color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+//         />
+//       );
+//     },
+//   },
+// },
+// PollCreation: {
+//   screen: PollCreation,
+//   navigationOptions: {
+//     tabBarLabel: "",
+//     tabBarOptions: {
+//       activeTintColor: '#1145FD',
+//     },
+//     tabBarIcon: (tabInfo) => {
+//       return (
+//         <Ionicons
+//           name="duplicate"
+//           size={24}
+//           color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+//         />
+//       );
+//     },
+//   },
+// },
+// Notification: {
+//   screen: NotificationScreen,
+//   navigationOptions: {
+//     tabBarLabel: "Notifications",
+//     tabBarOptions: {
+//       activeTintColor: '#1145FD',
+//     },
+//     tabBarIcon: (tabInfo) => {
+//       return (
+//         <Ionicons
+//           name="notifications"
+//           size={24}
+//           color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+//         />
+//       );
+//     },
+//   },
+// },
 
-Profile: {
-  screen: Profile,
-  navigationOptions: {
-    tabBarLabel: "Profile",
-    tabBarOptions: {
-      activeTintColor: '#1145FD',
-    },
-    tabBarIcon: (tabInfo) => {
-      return (
-        <Ionicons
-          name="person-circle"
-          size={24}
-          color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
-        />
-      );
-    },
-  },
-},
-});
+// Profile: {
+//   screen: Profile,
+//   navigationOptions: {
+//     tabBarLabel: "Profile",
+//     tabBarOptions: {
+//       activeTintColor: '#1145FD',
+//     },
+//     tabBarIcon: (tabInfo) => {
+//       return (
+//         <Ionicons
+//           name="person-circle"
+//           size={24}
+//           color={tabInfo.focused ? '#1145FD' : "#8e8e93"}
+//         />
+//       );
+//     },
+//   },
+// },
+// });
 
 
 
@@ -124,8 +124,26 @@ const BottomNavigation = () => {
         ></Image>
       </View>
     </KeyboardAvoidingView>
-    <Tab.Navigator>
-      
+    <Tab.Navigator
+    initialRouteName="Home"
+    tabBarOptions= {{
+    activeTintColor: '#1145FD',
+              }}
+    >
+     <Tab.Screen
+     name= "Home"
+     component={HomeScreen}
+     options={{
+     tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+             name="home"
+             size={24}
+            color={tabInfo.focused ?'#1145FD': "#8e8e93"}/>
+          )
+     }
+        }}
+     /> 
       
       
       
