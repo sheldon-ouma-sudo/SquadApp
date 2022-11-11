e1376976114f035a29f69cb97edb5023506f4e3a
sibuor-sudo <otieno1@stanford.edu>
2022-11-11 14:13:14 -0800

    adding all the changes

--- src/screens/PersonalClosedPollScreen.js
+++ src/screens/PersonalClosedPollScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const PersonalClosedPollScreen = () => {
@@ -8,5 +8,14 @@ const PersonalClosedPollScreen = () => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+  flex:1,
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor: "#fff"
+  },
+})
 
 export default PersonalClosedPollScreen
