7b4b2494904634abef16fbf7f9d174b3294d10ba
sibuor-sudo <otieno1@stanford.edu>
2022-11-23 13:57:51 -0800

    adding the map marker screen

---
+++ src/screens/MapMarkerSCreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MapMarkerSCreen = () => {
+  return (
+    <View>
+      <Text>MapMarkerSCreen</Text>
+    </View>
+  )
+}
+
+export default MapMarkerSCreen
