1e7d8f479fe3684916e657bca0a768ffb5451d33
sibuor-sudo <otieno1@stanford.edu>
2022-11-26 15:01:04 -0800

    adding all the changes

--- src/screens/MapMarkerSCreen.js
+++ src/screens/MapMarkerSCreen.js
@@ -12,35 +12,7 @@ const MapMarkerSCreen = () => {
     
   return (
     <View style={{ marginTop: 50, flex: 1 }}>
-    <GooglePlacesAutocomplete
-        placeholder="Search"
-        fetchDetails={true}
-        GooglePlacesSearchQuery={{
-            rankby: "distance"
-        }}
-        onPress={(data, details = null) => {
-            // 'details' is provided when fetchDetails = true
-            console.log(data, details)
-            setRegion({
-                latitude: details.geometry.location.lat,
-                longitude: details.geometry.location.lng,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-            })
-        }}
-        query={{
-            key: "AIzaSyCVlxhUjdMWqXtEpJhRImNEB9WAnp0uIWY",
-            language: "en",
-            components: "country:us",
-            types: "establishment",
-            radius: 30000,
-            location: `${region.latitude}, ${region.longitude}`
-        }}
-        styles={{
-            container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
-            listView: { backgroundColor: "white" }
-        }}
-    />
+   
     <MapView
         style={styles.map}
         initialRegion={{
@@ -49,7 +21,7 @@ const MapMarkerSCreen = () => {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421
         }}
-        provider="google"
+       // provider="google"
     >
         <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
         <Marker
