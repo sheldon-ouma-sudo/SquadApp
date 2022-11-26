4d54aeee78a828d2dadc01cbe3bd7cc8fff65cfb
sibuor-sudo <otieno1@stanford.edu>
2022-11-26 00:37:03 -0800

    adding the changes

--- src/screens/MapMarkerSCreen.js
+++ src/screens/MapMarkerSCreen.js
@@ -6,11 +6,84 @@ import { useState } from "react"
 
 
 const MapMarkerSCreen = () => {
+    const [ pin, setPin ] = useState({latitude: 37.78825,longitude: -122.4324})
+	const [ region, setRegion ] = useState({latitude: 37.78825,longitude: -122.4324,latitudeDelta: 0.0922,longitudeDelta: 0.0421})
+
+    
   return (
-    <View>
-      <Text>MapMarkerSCreen</Text>
-    </View>
+    <View style={{ marginTop: 50, flex: 1 }}>
+    <GooglePlacesAutocomplete
+        placeholder="Search"
+        fetchDetails={true}
+        GooglePlacesSearchQuery={{
+            rankby: "distance"
+        }}
+        onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details)
+            setRegion({
+                latitude: details.geometry.location.lat,
+                longitude: details.geometry.location.lng,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            })
+        }}
+        query={{
+            key: "AIzaSyCVlxhUjdMWqXtEpJhRImNEB9WAnp0uIWY",
+            language: "en",
+            components: "country:us",
+            types: "establishment",
+            radius: 30000,
+            location: `${region.latitude}, ${region.longitude}`
+        }}
+        styles={{
+            container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+            listView: { backgroundColor: "white" }
+        }}
+    />
+    <MapView
+        style={styles.map}
+        initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        }}
+        provider="google"
+    >
+        <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+        <Marker
+            coordinate={pin}
+            pinColor='#1145FD'
+            draggable={true}
+            onDragStart={(e) => {
+                console.log("Drag start", e.nativeEvent.coordinates)
+            }}
+            onDragEnd={(e) => {
+                setPin({
+                    latitude: e.nativeEvent.coordinate.latitude,
+                    longitude: e.nativeEvent.coordinate.longitude
+                })
+            }}
+        >
+           
+        </Marker>
+        <Circle center={pin} radius={1000} />
+    </MapView>
+</View>
   )
 }
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height
+	}
+})
 export default MapMarkerSCreen
