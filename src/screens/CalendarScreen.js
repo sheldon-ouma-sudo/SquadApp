daa2f9ad5afd18e86f9fc99c5b1955c99f5af078
sibuor-sudo <otieno1@stanford.edu>
2022-11-29 03:23:22 -0800

    adding all the changes on the calendar development

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -13,6 +13,7 @@ import { View, Text,KeyboardAvoidingView,Image, StyleSheet,
     //import {BlurView} from '@react-native-community/blur';
     import { BlurView } from 'expo-blur';
     import {Calendar } from 'react-native-calendars'
+    import moment from 'moment';
 
   
   
@@ -22,32 +23,85 @@ const CalendarScreen = () => {
 //   const[dateOfBirth, setDateOfBirth] = useState("MM-DD-YY")
 //   const [selectedGender, setGenderSelected] =useState("");
 //   const[location, setLocation]=useState("Enter your location")
-
-
-
+const [markedDates, setMarkedDates] = useState({
+    //'2021-01-20': {textColor: 'green'},
+    //'2021-01-22': {startingDay: true, color: 'green'},
+    //'2021-01-23': {selected: true, endingDay: true, color: 'green', textColor: 'gray'},
+    //'2021-01-04': {disabled: true, startingDay: true, color: 'green', endingDay: true}
+    '2022-11-28': {selected: true,  selectedColor: '#1145FD'},
+        
+      
+  })
   const navigation = useNavigation()
   const route = useRoute();
   const inputRef = useRef()
-  
-    
   const uri = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png';
 
   //const username = route?.params.username 
-  const text = 'Hello, my container is blurring contents underneath!';
+  //const text = 'Hello, my container is blurring contents underneath!';
+  const handleDayPress = (day) => {
+    setMarkedDates({
+      [day.dateString]: {
+        color: '#1145FD'
+      },
+    //   [moment(day.dateString).add(1, 'days').format('YYYY-MM-DD')]: {
+    //     color: 'green'
+    //   },
+    //   [moment(day.dateString).add(2, 'days').format('YYYY-MM-DD')]: {
+    //     color: 'green'
+    //   },
+    //   [moment(day.dateString).add(3, 'days').format('YYYY-MM-DD')]: {
+    //     endingDay: true,color: 'green'
+    //   }
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
       
      
       <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
-        <Text style={[styles.text, { color: '#fff' }]}>{text}</Text>
-        <Calendar
-           markingType = {'custom'}
-           markedDates={{
-            '2012-05-16': {selected: true, marked: true, selectedColor: '#1145FD'},
-            
-          }}
-         />
+      <Calendar
+      markedDates={markedDates}
+      markingType={'period'}
+      onDayPress={handleDayPress}
+      style={{
+        borderWidth: 1,
+        borderColor: 'gray',
+        height: 350,
+        borderRadius:10,
+        arrowColor: 'black',
+        
+      }}
+      // Specify theme properties to override specific styles for calendar parts. Default = {}
+    //   theme={{
+    //     backgroundColor: '#ffffff',
+    //     calendarBackground: '#ffffff',
+    //     textSectionTitleColor: '#b6c1cd',
+    //     textSectionTitleDisabledColor: '#d9e1e8',
+    //     selectedDayBackgroundColor: '#00adf5',
+    //     selectedDayTextColor: '#ffffff',
+    //     todayTextColor: '#00adf5',
+    //     dayTextColor: '#2d4150',
+    //     textDisabledColor: '#d9e1e8',
+    //     dotColor: '#00adf5',
+    //     selectedDotColor: '#ffffff',
+    //     arrowColor: 'black',
+    //     disabledArrowColor: '#d9e1e8',
+    //     monthTextColor: 'blue',
+    //     indicatorColor: 'blue',
+    //     textDayFontFamily: 'monospace',
+    //     textMonthFontFamily: 'monospace',
+    //     textDayHeaderFontFamily: 'monospace',
+    //     textDayFontWeight: '300',
+    //     textMonthFontWeight: 'bold',
+    //     textDayHeaderFontWeight: '300',
+    //     textDayFontSize: 16,
+    //     textMonthFontSize: 16,
+    //     textDayHeaderFontSize: 16
+    //   }}
+      />
       </BlurView>
     </View>
   );
