63c1a5b035fd972a26baf7fee5ba3e57c817e4c8
sibuor-sudo <otieno1@stanford.edu>
2022-11-29 19:26:06 -0800

    adding all the dependencies

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3squadfilestorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
