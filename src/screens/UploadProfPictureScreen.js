41564beeeabf1ca757f3c8461ece3ddedf1baeec
sibuor-sudo <otieno1@stanford.edu>
2022-12-07 18:48:28 -0800

    adding all the changes

--- src/screens/UploadProfPictureScreen.js
+++ src/screens/UploadProfPictureScreen.js
@@ -50,21 +50,21 @@ const UploadProfPicture = () => {
   },[])
 
 
-const pickImage = async () => {
-  let result = await ImagePicker.launchImageLibraryAsync({
-    meadiaTypes:ImagePicker.MediaTypeOptions.Images,
-    allowsEditing: true, 
-    aspect:[4,3],
-    quality:1,
-  });
-  console.log(result)
-  if(!result.cancelled){
-    setImage(result.uri)
-  }
-  if(hasGalleryPermissions===false){
-    return<Text>No access to photo gallery</Text>
-  }
-}
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
 const takePhotoFromCamera = () =>{
 ImagePicker.launchCameraAsync({
