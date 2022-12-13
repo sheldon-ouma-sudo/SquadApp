de78f4219538b713523c6d85fb14f8397551d605
sibuor-sudo <otieno1@stanford.edu>
2022-12-13 01:38:47 -0500

    changing the UI of the personal interest

--- src/screens/PersonalInterests.js
+++ src/screens/PersonalInterests.js
@@ -2,12 +2,13 @@
     StatusBar,Dimensions,SafeAreaView,SectionList,FlatList} from 'react-native'
     import React, { useEffect, useState } from 'react'
     import StepIndicator from 'react-native-step-indicator';
-     import { TouchableOpacity } from 'react-native';
-     import { useNavigation } from '@react-navigation/native';
-     
-    //const labels = ["Cart","Delivery Address","Order Summary","Payment Method","Track"];
+    import { TouchableOpacity } from 'react-native';
+    import { useNavigation } from '@react-navigation/native';
+    import Constants from 'expo-constants';
+
+   
     const{width,height} = Dimensions.get("window")
-    //const[currentPosition, setCurrentPositon]=useState(0)
+
     const customStyles = {
       stepIndicatorSize: 25,
       currentStepIndicatorSize:30,
@@ -33,101 +34,77 @@
     }
 
     
+    const DATA = [
+      {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'Food',
+        url: 'https://media3.giphy.com/media/26xBzfqV1XqKAlRCw/giphy.gif',
+      },
+      {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Fashion',
+        url: 'https://media3.giphy.com/media/xjIh4zHDjhjji/giphy.gif',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Travel',
+        url: 'https://media3.giphy.com/media/iBBfBIj1XopJF6WTVI/giphy.gif',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72v2',
+        title: 'Decor',
+         url: 'https://media3.giphy.com/media/JGdbbSyi3wM9uBKv8p/giphy.gif',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72v3',
+        title: 'Wellness',
+        url: 'https://media3.giphy.com/media/cAgGLp84BRh4lZumDt/giphy.gif',
+      },
+      {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72v4',
+        title: 'Social',
+        url: 'https://media3.giphy.com/media/Swg9cud2W8OKVhz9rt/giphy.gif',
     
+      },
+    
+    ];
+   
+    function Item({ id, title, selected, onSelect,url}) {
+      return (
+        <TouchableOpacity 
+          onPress={() => onSelect(id)}
+          style={[
+            styles.item,
+            { backgroundColor: selected ? '#1145FD' : '#fff'  },
+          ]}
+        >
+          <Image
+          source={{uri:url}}
+          style={styles.itemPhoto}
+          resizeMode="cover"
+        />
+    
+          <Text style={[styles.title,{color:selected?'#fff':'#1145FD'}]}>{title}</Text>
+        </TouchableOpacity>
+      );
+    }
      
 const PersonalInterests = () => {
-  const navigation = useNavigation()
+  const [selected, setSelected] = useState(new Map());
   const[currentPosition, setCurrentPositon] = useState(2)
-  const[personalInterest, setPersonalInterest]=useState([])
-  const [open, setOpen] = React.useState(false)
-  const [data, setData] = React.useState([
-    { label: 'temperature', selected:false },
-    { label: 'humidity', },
-    { label: 'light', },
-    { label: 'move', },
-    { label: 'sound', },
-    { label: 'carbon dioxide', },
-    { label: 'air pollution', }
-  ])
+  const navigation = useNavigation()
+  const onSelect = useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
 
-  const SECTIONS = [
-    {
-      title: 'Select the themes you are intersted in:',
-      data: [
-        {
-          key: '1',
-          text: 'Food',
-          uri: 'https://media3.giphy.com/media/26xBzfqV1XqKAlRCw/giphy.gif',
-        },
-        {
-          key: '2',
-          text: 'Fashion',
-          uri: 'https://media3.giphy.com/media/xjIh4zHDjhjji/giphy.gif',
-         
-        },
-  
-        {
-          key: '3',
-          text: 'Travel',
-          uri: 'https://media3.giphy.com/media/iBBfBIj1XopJF6WTVI/giphy.gif',
-        },
-        
-      ],
+      setSelected(newSelected);
     },
-    {
-      //title: 'Punk and hardcore',
-      data: [
-        {
-          key: '4',
-          text: 'Decor',
-          uri: 'https://media3.giphy.com/media/JGdbbSyi3wM9uBKv8p/giphy.gif',
-        },
-        {
-          key: '5',
-          text: 'Wellness',
-          uri: 'https://media3.giphy.com/media/cAgGLp84BRh4lZumDt/giphy.gif',
-        },
-        {
-          key: '6',
-          text: 'Social',
-          uri: 'https://media3.giphy.com/media/Swg9cud2W8OKVhz9rt/giphy.gif',
-        },
-    
-      ],
-    
-     
-    },
-  ];
-  openList = () => setOpen(true)
-  closeList = () => setOpen(false)
-  onUpdateValue = (index, value) => { data[index].selected = value; return setData([...data]);}
-  renderItem = ({ item, index }) => <ItemRenderer key={index} index={index} selected={item.selected} label={item.label} onUpdateValue={onUpdateValue} />
-  const ListItem = ({ item }) => {
-    return(
-    <TouchableOpacity 
-    onPress={()=>{setPersonalInterest([...personalInterest,item])
-      console.log(personalInterest)
-      console.log(item)
-      console.log("personal intereste is as follows: ",personalInterest)
-    }
-  
-  }
-    >
-    <View style={[styles.item,{marginLeft:15},{marginRight:-2}]}>
-    <Image
-      source={{
-        uri: item.uri,
-      }}
-      style={styles.itemPhoto}
-      resizeMode="cover"
-    />
-    </View>
-    <Text style={styles.itemText}>{item.text}</Text>
-  </TouchableOpacity>
-  )};
-  
+    [selected],
+  );
+
   return (
-    <KeyboardAvoidingView 
+    <SafeAreaView
     style={styles.container}
     behavior="padding"
     >
@@ -148,90 +125,105 @@ const PersonalInterests = () => {
      currentPosition={currentPosition}
      //labels={labels}
      />
+     <View
+     style={[{marginTop:10},{marginBottom:10}]}
+     >
+      <Text
+      style={styles.textInterest}
+      > 
+      Select themes are you are interested:
+      </Text>
+     </View>
     </View>  
-    <View style={{marginTop:-30}}>
-      <SafeAreaView>
-          <SectionList
-            contentContainerStyle={{ paddingHorizontal: 10 }}
-            stickySectionHeadersEnabled={false}
-            sections={SECTIONS}
-            renderSectionHeader={({ section }) => (
-                    <>
-              <Text style={styles.sectionHeader}>{section.title}</Text>
-              <FlatList
-                horizontal
-                data={section.data}
-                renderItem={({ item }) => 
-               <ListItem item={item} />  
-                }
-                contentContainerStyle={{ paddingBottom: -40 }}
-                showsHorizontalScrollIndicator={false}
-                />
-                </>
-                )}
-                renderItem={({ item, section }) => {
-                  return null;
-                  //return <ListItem item={item} />;
-                }}
-                />
-                  <TouchableOpacity  onPress={() =>navigation.replace('UploadProfPictureScreen')}style={[ styles.backButton,{borderColor:'#1145FD'}, {marginBottom:-140},{marginLeft:40},{marginTop:-330} ]}>
-                  <Text  style={[{justifyContent: 'flex-end'},styles.backText]}> Back </Text></TouchableOpacity>
-                  <TouchableOpacity  onPress={() =>navigation.replace('SquadCreationScreen')}style={[ styles.button,{borderColor:'#1145FD'}, {marginBottom:300},{marginLeft:230}, ]}>
-                  <Text  style={[{justifyContent: 'flex-end'},styles.buttonText]}> Next </Text></TouchableOpacity>
-                </SafeAreaView>
-        </View>
-        
-    </KeyboardAvoidingView>
-  )
+    <View style={{marginTop:-30}}></View>
+       <ScrollView horizontal={true} showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}
+       style={[{marginRight:20}]}
+       >
+      <FlatList
+         data={DATA}
+         scrollEnabled={false}
+         numColumns={Math.ceil(DATA.length / 2)}
+        renderItem={({ item }) => (
+          <Item
+            id={item.id}
+            title={item.title}
+            url = {item.url}
+            selected={!!selected.get(item.id)}
+            onSelect={onSelect}
+          />
+        )}
+        keyExtractor={item => item.id}
+        extraData={selected}
+      />
+      </ScrollView>
+
+      <TouchableOpacity  onPress={() =>navigation.replace('UploadProfPictureScreen')}style={[ styles.backButton,{borderColor:'#1145FD'}, {marginBottom:-140},{marginLeft:70},{marginTop:-330},{marginRight:250},{width:160} ]}>
+      <Text  style={[{justifyContent: 'flex-end'},styles.backText,]}> Back </Text>
+      </TouchableOpacity>
+      <TouchableOpacity  onPress={() =>navigation.replace('SquadCreationScreen')}style={[ styles.button,{borderColor:'#1145FD'}, {marginBottom:80},{marginLeft:230},{marginRight:50},{width:160} ]}>
+      <Text  style={[{justifyContent: 'flex-end'},styles.buttonText]}> Next </Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-  container:{
-  flex:1,
-  justifyContent:"flex-start",
-  alignItems:"center",
-  backgroundColor: "#F4F8FB",
-  
-
+  container: {
+    flex: 1,
+    alignItems:"center",
+    justifyContent:"flex-start",
+    marginTop: Constants.statusBarHeight,
   },
   squadLogo:{
-      width:100,
-      height:35,
-      marginRight:250,
-      marginTop:70
-      
+    width:100,
+    height:35,
+    marginRight:250,
+    marginTop:70
+},
 
-   
+header:{
+  height: 55, 
+  width:'50%',
+  justifyContent:"center",
+  alignItems:'center',
+  marginRight:200,
+  marginTop: 10,
+  marginLeft:35
+},
+headerText:{
+  fontSize:22,
+  fontWeight:'bold'
+},
+indicatiorWindow:{
+  width:width-20,
+  padding:20,
+  margin:15,
+  borderRadius:20,
+  marginTop:-20,
+  marginLeft:-8
+},
+textInterest:{
+fontSize:15
+},
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal:10,
+    height:120,
+    width:120,
+    borderRadius:3,
+    marginRight:-5
   },
-  header:{
-    height: 55, 
-    //padding:10, 
-    width:'50%',
-    //backgroundColor:"#000",
-    //elevation:10,
-    justifyContent:"center",
-    alignItems:'center',
-    marginRight:200,
-    marginTop: 10,
-    marginLeft:35
+  title: {
+    fontSize: 12,
+    marginTop:5,
+    fontWeight:'600'
   },
-  headerText:{
-    //color:'red',
-    fontSize:22,
-    fontWeight:'bold'
-  },
-  indicatiorWindow:{
-    //height:height-170,
-    width:width-20,
-    padding:20,
-    margin:15,
-    //elevation:10,
-    borderRadius:20,
-    marginTop:-20,
-    marginLeft:-8
-
-    //backgroundColor:'blue'
+  itemPhoto: {
+    width: 75,
+    height: 75,
+    alignSelf:'center'
   },
   button:{
     backgroundColor: '#1145FD',
@@ -248,82 +240,31 @@ const styles = StyleSheet.create({
 
 backButton:{
   backgroundColor: '#EAEAEA',
-  width: 180,
+  width: 150,
   height: 42,
   padding: 10,
   borderRadius: 5,
-  //marginTop: 100,
-  alignItems: 'center',
-  //marginRight: 5,
-  //marginLeft:10,
+  alignItems: 'center', 
   borderColor:'#1145FD'
-
-
 },
 buttonText:{
   color: 'white',
   fontWeight: '700',
   fontSize: 15,
-  alignItems:"center"
-  
-  
+  alignItems:"center",
+  width:50 
 },
 backText:{
   color: '#1145FD',
   fontWeight: '700',
   fontSize: 15,
   alignItems:"center"
-  
-  
 },
 nextText:{
   color: '#1145FD',
   fontWeight: '700',
   fontSize: 15,
   alignItems:"center"
-  
-  
 },
-  
-profilePictureButton:{
-  backgroundColor: '#1145FD',
-  width: 256,
-  height: 42,
-  padding: 10,
-  borderRadius: 5,
-  marginTop: 30,
-  alignItems: 'center',
-  marginRight: 50,
-  marginLeft:50,
-
-},
-sectionHeader: {
-  fontWeight: '400',
-  fontSize: 15,
-  //color: '#f4f4f4',
-  marginTop: 10,
-  marginBottom: 5,
-  marginLeft:25
-  //padding:-15,
-
- 
-},
-item: {
-  margin: 10,
-  backgroundColor: '#FFFFFF',
-  padding:10,
-  borderRadius:5
-},
-itemPhoto: {
-  width: 100,
-  height: 100,
-},
-itemText: {
-  color: '#1145FD',
-  marginTop: 8,
-  fontWeight:'600',
-  marginLeft:20
-},  
-
-  },)
-  export default PersonalInterests
+});
+ export default PersonalInterests
--- src/screens/TestWorkScreen.js
+++ src/screens/TestWorkScreen.js
@@ -5,40 +5,74 @@ import {
   FlatList,
   StyleSheet,
   Text,
-  SectionList,
+ ScrollView,
+ Image,
+ View,
+ StatusBar,
+ Dimensions,
 } from 'react-native';
+import StepIndicator from 'react-native-step-indicator';
+import { useState } from 'react';
+import { useCallback } from 'react';
 import Constants from 'expo-constants';
+import { useNavigation } from '@react-navigation/native';
+
+
+const{width,height} = Dimensions.get("window")
+const customStyles = {
+  stepIndicatorSize: 25,
+  currentStepIndicatorSize:30,
+  separatorStrokeWidth: 2, 
+  currentStepStrokeWidth: 3,
+  stepStrokeCurrentColor: '#ffff',
+  stepStrokeWidth: 3,
+  stepStrokeFinishedColor: '#1764EF',
+  stepStrokeUnFinishedColor: '#aaaaaa',
+  separatorFinishedColor: '#1764EF',
+  separatorUnFinishedColor: '#aaaaaa',
+  stepIndicatorFinishedColor:  '#1764EF',
+  stepIndicatorUnFinishedColor: '#ffffff',
+  stepIndicatorCurrentColor: '#1764EF',
+  stepIndicatorLabelFontSize: 13,
+  currentStepIndicatorLabelFontSize: 13,
+  stepIndicatorLabelCurrentColor: '#ffffff',
+  stepIndicatorLabelFinishedColor: '#ffffff',
+  stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+  //labelColor: '#999999',
+  labelSize: 13,
+  currentStepLabelColor: '#fffff'
+}
 
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    title: 'Food',
     url: 'https://media3.giphy.com/media/26xBzfqV1XqKAlRCw/giphy.gif',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-    uri: 'https://media3.giphy.com/media/xjIh4zHDjhjji/giphy.gif',
+    title: 'Fashion',
+    url: 'https://media3.giphy.com/media/xjIh4zHDjhjji/giphy.gif',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-    uri: 'https://media3.giphy.com/media/iBBfBIj1XopJF6WTVI/giphy.gif',
+    title: 'Travel',
+    url: 'https://media3.giphy.com/media/iBBfBIj1XopJF6WTVI/giphy.gif',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72v2',
-    title: 'Fourth Item',
-     uri: 'https://media3.giphy.com/media/JGdbbSyi3wM9uBKv8p/giphy.gif',
+    title: 'Decor',
+     url: 'https://media3.giphy.com/media/JGdbbSyi3wM9uBKv8p/giphy.gif',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72v3',
-    title: 'Fifth Item',
-    uri: 'https://media3.giphy.com/media/cAgGLp84BRh4lZumDt/giphy.gif',
+    title: 'Wellness',
+    url: 'https://media3.giphy.com/media/cAgGLp84BRh4lZumDt/giphy.gif',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72v4',
-    title: 'Sixth Item',
-    uri: 'https://media3.giphy.com/media/Swg9cud2W8OKVhz9rt/giphy.gif',
+    title: 'Social',
+    url: 'https://media3.giphy.com/media/Swg9cud2W8OKVhz9rt/giphy.gif',
 
   },
 
@@ -46,24 +80,31 @@ const DATA = [
 
 
 
-function Item({ id, title, selected, onSelect }) {
+function Item({ id, title, selected, onSelect,url}) {
   return (
-    <TouchableOpacity
+    <TouchableOpacity 
       onPress={() => onSelect(id)}
       style={[
         styles.item,
-        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+        { backgroundColor: selected ? '#1145FD' : '#fff'  },
       ]}
     >
-      <Text style={styles.title}>{title}</Text>
+      <Image
+      source={{uri:url}}
+      style={styles.itemPhoto}
+      resizeMode="cover"
+    />
+
+      <Text style={[styles.title,{color:selected?'#fff':'#1145FD'}]}>{title}</Text>
     </TouchableOpacity>
   );
 }
 
 const TestWorkScreen =() => {
-  const [selected, setSelected] = React.useState(new Map());
-
-  const onSelect = React.useCallback(
+  const [selected, setSelected] = useState(new Map());
+  const[currentPosition, setCurrentPositon] = useState(2)
+  const navigation = useNavigation()
+  const onSelect = useCallback(
     id => {
       const newSelected = new Map(selected);
       newSelected.set(id, !selected.get(id));
@@ -74,13 +115,50 @@ const TestWorkScreen =() => {
   );
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView
+    style={styles.container}
+    behavior="padding"
+    >
+    <View style={[styles.squadLogoContainer, {flexDirection:'column'}]}>
+      <Image
+        source={require('/Users/sheldonotieno/Squad/assets/squad-logo.png')}
+        style={styles.squadLogo}
+        resizeMode='contain'
+      ></Image>
+    </View>      
+    <StatusBar backgroundColor={'black'} barStyle="light-content" />
+    <View style={styles.header}>
+      <Text style={styles.headerText}> Sign Up Progress</Text>
+    </View>
+    <View style={styles.indicatiorWindow}>
+    <StepIndicator
+     customStyles={customStyles}
+     currentPosition={currentPosition}
+     //labels={labels}
+     />
+     <View
+     style={[{marginTop:10},{marginBottom:10}]}
+     >
+      <Text
+      style={styles.textInterest}
+      > 
+      Select themes are you are interested:
+      </Text>
+     </View>
+    </View>  
+    <View style={{marginTop:-30}}></View>
+       <ScrollView horizontal={true} showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}
+       style={[{marginRight:20}]}
+       >
       <FlatList
          data={DATA}
+         scrollEnabled={false}
+         numColumns={Math.ceil(DATA.length / 2)}
         renderItem={({ item }) => (
           <Item
             id={item.id}
             title={item.title}
+            url = {item.url}
             selected={!!selected.get(item.id)}
             onSelect={onSelect}
           />
@@ -88,7 +166,15 @@ const TestWorkScreen =() => {
         keyExtractor={item => item.id}
         extraData={selected}
       />
-      
+      </ScrollView>
+
+      <TouchableOpacity  onPress={() =>navigation.replace('UploadProfPictureScreen')}style={[ styles.backButton,{borderColor:'#1145FD'}, {marginBottom:-140},{marginLeft:70},{marginTop:-330},{marginRight:250},{width:160} ]}>
+      <Text  style={[{justifyContent: 'flex-end'},styles.backText,]}> Back </Text>
+      </TouchableOpacity>
+      <TouchableOpacity  onPress={() =>navigation.replace('SquadCreationScreen')}style={[ styles.button,{borderColor:'#1145FD'}, {marginBottom:80},{marginLeft:230},{marginRight:50},{width:160} ]}>
+      <Text  style={[{justifyContent: 'flex-end'},styles.buttonText]}> Next </Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 }
@@ -96,16 +182,100 @@ const TestWorkScreen =() => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems:"center",
+    justifyContent:"flex-start",
     marginTop: Constants.statusBarHeight,
   },
+  squadLogo:{
+    width:100,
+    height:35,
+    marginRight:250,
+    marginTop:70
+},
+
+header:{
+  height: 55, 
+  width:'50%',
+  justifyContent:"center",
+  alignItems:'center',
+  marginRight:200,
+  marginTop: 10,
+  marginLeft:35
+},
+headerText:{
+  fontSize:22,
+  fontWeight:'bold'
+},
+indicatiorWindow:{
+  width:width-20,
+  padding:20,
+  margin:15,
+  borderRadius:20,
+  marginTop:-20,
+  marginLeft:-8
+},
+textInterest:{
+fontSize:15
+},
   item: {
-    backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal:10,
+    height:120,
+    width:120,
+    borderRadius:3,
+    marginRight:-5
   },
   title: {
-    fontSize: 32,
+    fontSize: 12,
+    marginTop:5,
+    fontWeight:'600'
   },
+  itemPhoto: {
+    width: 75,
+    height: 75,
+    alignSelf:'center'
+  },
+  button:{
+    backgroundColor: '#1145FD',
+    width: 180,
+    height: 42,
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 100,
+    alignItems: 'center',
+    marginRight: 30,
+    marginLeft:20,
+
+},
+
+backButton:{
+  backgroundColor: '#EAEAEA',
+  width: 150,
+  height: 42,
+  padding: 10,
+  borderRadius: 5,
+  alignItems: 'center', 
+  borderColor:'#1145FD'
+},
+buttonText:{
+  color: 'white',
+  fontWeight: '700',
+  fontSize: 15,
+  alignItems:"center",
+  width:50 
+},
+backText:{
+  color: '#1145FD',
+  fontWeight: '700',
+  fontSize: 15,
+  alignItems:"center"
+},
+nextText:{
+  color: '#1145FD',
+  fontWeight: '700',
+  fontSize: 15,
+  alignItems:"center"
+},
 });
 export default TestWorkScreen
