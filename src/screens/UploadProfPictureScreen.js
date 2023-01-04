81c0c947a149cd6dbb408999c7cc8f8762d65d65
sibuor-sudo <otieno1@stanford.edu>
2023-01-03 22:59:23 -0800

    adding the changes on the profile upload section

--- src/screens/UploadProfPictureScreen.js
+++ src/screens/UploadProfPictureScreen.js
@@ -94,7 +94,7 @@ const fetchResourceFromURI = async uri => {
   return blob;
 };
 //upload the picture to the specific bucket
-
+useEffect(()=>{})
  const uploadUserImage = async () => {
   alert("uploading the photo attempt")
     if (isLoading) return;
