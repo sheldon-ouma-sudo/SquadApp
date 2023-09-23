b2d433455cc9f019d032c414aaed7a90eb7e4122
sibuor-sudo <otieno1@stanford.edu>
2023-09-23 14:56:44 -0700

    done designing the profile page and started on working the front end of the edit profile

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,KeyboardAvoidingView,Image, StyleSheet, 
+  StatusBar,Dimensions,TouchableOpacity, SafeAreaView} from 'react-native'
+  import React, { useEffect, useState } from 'react'
+  import StepIndicator from 'react-native-step-indicator';
+  //import { Icon } from 'react-native-elements';
+  //import Ionicons from '@expo/vector-icons/Ionicons';
+  import { FontAwesome } from '@expo/vector-icons'; 
+  import { Entypo } from '@expo/vector-icons'; 
+  import { useNavigation } from '@react-navigation/native';
+
+  const{width,height} = Dimensions.get("window")
+
+  const customStyles = {
+    stepIndicatorSize: 25,
+    currentStepIndicatorSize:30,
+    separatorStrokeWidth: 2, 
+    currentStepStrokeWidth: 3,
+    stepStrokeCurrentColor: '#ffff',
+    stepStrokeWidth: 3,
+    stepStrokeFinishedColor: '#ffff',
+    stepStrokeUnFinishedColor: '#aaaaaa',
+    separatorFinishedColor: '#ffff',
+    separatorUnFinishedColor: '#aaaaaa',
+    stepIndicatorFinishedColor: '#fff',
+    stepIndicatorUnFinishedColor: '#ffffff',
+    stepIndicatorCurrentColor: '#1764EF',
+    stepIndicatorLabelFontSize: 13,
+    currentStepIndicatorLabelFontSize: 13,
+    stepIndicatorLabelCurrentColor: '#ffffff',
+    stepIndicatorLabelFinishedColor: '#ffffff',
+    stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+    labelColor: '#999999',
+    labelSize: 13,
+    currentStepLabelColor: '#fffff'
+  }
+
 
 const EditProfileScreen = () => {
   return (
-    <View>
+    <SafeAreaView
+    style={styles.container}
+        behavior="padding">
       <Text>EditProfile</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  )}
+  const styles = StyleSheet.create({
+    container:{
+    flex:1,
+    justifyContent:"flex-start",
+    alignItems:"center",
+    backgroundColor: "#F4F8FB"
+  
+    },
+  })
 
 export default EditProfileScreen
--- src/screens/ProfileTopNavigation.js
+++ src/screens/ProfileTopNavigation.js
@@ -3,7 +3,9 @@
   import PersonalPollScreen from './PersonalPollScreen'
   import SwayingScreen from './SwayingScreen'
   import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+  import { useNavigation } from '@react-navigation/native'
   import MySquadScreen from './MySquadScreen'
+  import { AntDesign } from '@expo/vector-icons';
   import React from 'react'
   import { useState, useEffect } from 'react'
   import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -15,11 +17,12 @@
 
   const Profile =()=> {
     const[profileImage, setProflieImage]= useState('https://squad-file-storage235821-staging.s3.us-west-2.amazonaws.com/Squad_inApp_images/userProfilePlaceholder.png')
-    const[userName, setUserName] =useState('Name')
-    //const[userProfilePicture, setUserProfilePicture] = useState("")
-    const[numOfUserPolls, setNumOfUserPolls] = useState("")
-    const[numOfUserSways, setNumOfUserSways] = useState("0")
+    const[userName, setUserName] =useState('User Name')
+    const[numOfUserPolls, setNumOfUserPolls] = useState("1060")
+    const[numOfUserSquadron, setNumOfUserSways] = useState("1060")
+    const[numOfUsersInSquad, setNumOfSquadUsers] = useState("1060")
     const Tab = createMaterialTopTabNavigator();
+    const navigation = useNavigation()
     const insets = useSafeAreaInsets();  
     //query the user from the backend
     //set the values to what is in the backend 
@@ -55,13 +58,59 @@
     return (
       <>
       <View style={[{backgroundColor:"#F4F8FB"},{flexDirection:"row"}]}>
-        <TouchableOpacity>
+
+        <View style={{flex:1, justifyContent:'flex-start'}}>
           <Image
             source={require('/Users/sheldonotieno/Squad/assets/person-circle-sharp-pngrepo-com.png')}
             resizeMode={'contain'}
-            style={[{ height: 90 }, { width: 90 }, 
-            {overflow:'hidden'},{marginBottom:2}, {marginLeft:20},{marginTop:50}]} />
+            style={[{ height: 80 }, { width: 80 }, 
+            {overflow:'hidden'},{marginBottom:12}, {marginLeft:20},{marginTop:50}, {borderRadius:50}, {borderWidth:5}, {borderColor:'#7399DE'}]} />
+        </View> 
+        <View
+        style={{flex:1, justifyContent:'flex-end', marginBottom:25, marginEnd:30,marginLeft:-55}}
+        >
+        <TouchableOpacity
+        style={{marginStart:150,marginBottom:15}}
+        onPress={() =>navigation.navigate('EditProfileScreen')}
+        >
+        <AntDesign name="edit" size={24} color="black" />
         </TouchableOpacity>
+
+        <View
+        style={{marginLeft:-10,marginBottom:12.5}}
+        >
+          <Text
+          style={{fontWeight:'bold'}}
+          >{userName}</Text>
+        </View>
+       {/**this view here is for the numbers */}
+         <View>
+           <Text
+           style={{marginBottom:-15,marginLeft:-15,fontWeight:'bold'}}
+           >{numOfUserPolls}</Text> 
+           <Text
+           style={{marginLeft:50,fontWeight:'bold'}}
+           >{numOfUsersInSquad}</Text>  
+          <Text
+          style={{marginLeft:120,marginTop:-15,fontWeight:'bold'}}
+          >{numOfUserSquadron}</Text>
+         </View>
+          {/**this view here is for the labelling */}
+         <View
+         style={{marginLeft:-20}}
+         >
+           <Text
+           style={{marginBottom:-15,marginLeft:7,color:'#707070',fontWeight:'400'}}
+           >Polls</Text> 
+           <Text
+           style={{marginLeft:60,marginBottom:-15,color:'#707070',fontWeight:'400'}}
+           >Squad</Text>  
+          <Text
+          style={{marginLeft:125,color:'#707070',fontWeight:'400'}}
+          >Squadron</Text>
+         </View>
+        </View>
+        {/* 
         <View 
          style = {[{marginTop: 30}, {marginLeft:10}]}
         >
@@ -102,7 +151,7 @@
            style={[{fontSize:16},{marginTop:-23.5},{marginLeft:65}]}
            >Squadron
            </Text>
-        </View>
+        </View> */}
       </View>
 
 
