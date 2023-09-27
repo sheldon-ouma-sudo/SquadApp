bcfdf554558ed1e2abac8e7e19820059bbabc87e
sibuor-sudo <otieno1@stanford.edu>
2023-09-27 02:00:35 -0700

    adding the changes from the backend to the frontend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3squadstoragec4a7afa9": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
