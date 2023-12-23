85160404f7f31ee4322591426be3439070a8bec5
sibuor-sudo <otieno1@stanford.edu>
2023-12-23 01:56:34 -0800

    adding all the changes

--- src/components/PollItem/index.js
+++ src/components/PollItem/index.js
@@ -1,76 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
-
-const ProgressBar = ({ progress }) => {
-  const [loaderValue, setLoaderValue] = useState(0);
-
-  useEffect(() => {
-    Animated.timing(loaderValue, {
-      toValue: progress,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-  }, [progress]);
-
-  return (
-    <View style={styles.progressBar}>
-      <Animated.View style={[styles.loader, { width: loaderValue }]} />
-    </View>
-  );
-};
-
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const Poll = () => {
-  const [data, setData] = useState([
-    { id: 1, text: 'Item 1' },
-    { id: 2, text: 'Item 2' },
-    { id: 3, text: 'Item 3' },
-    { id: 4, text: 'Item 4' },
-    { id: 5, text: 'Item 5' },
-  ]);
-
-  const renderItem = ({ item }) => {
-    return (
-      <View style={styles.item}>
-        <Text>{item.text}</Text>
-      </View>
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        ListHeaderComponent={<ProgressBar progress={0.5} />}
-      />
+    <View>
+      <Text>index</Text>
     </View>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  item: {
-    padding: 10,
-    height: 40,
-    backgroundColor: '#fff',
-    borderRadius: 4,
-    margin: 5,
-  },
-  progressBar: {
-    height: 20,
-    backgroundColor: '#ccc',
-    borderRadius: 4,
-  },
-  loader: {
-    height: 20,
-    backgroundColor: '#000',
-    borderRadius: 4,
-  },
-});
-
-export default Poll;
+export default Poll
--- src/components/PollListItem/index.js
+++ src/components/PollListItem/index.js
@@ -1,202 +1,87 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
 
-    import { View, Text,KeyboardAvoidingView,Image, StyleSheet, 
-    StatusBar,Dimensions,SafeAreaView,SectionList,FlatList, Pressable, Button,TextInput, Animated} from 'react-native'
-    import React, { useEffect, useState } from 'react'
-    import { useNavigation } from '@react-navigation/native';
-    import {Auth, API, graphqlOperation} from "aws-amplify";
-    import { FontAwesome } from '@expo/vector-icons'; 
-    import RNAnimated from "react-native-animated-component";
-    import RNPoll, { IChoice } from "react-native-poll";
-    import Poll from 'react-native-poll';
-    //import {getPoll} from '../graphql/queries';
+const PollListItem = ({ poll, }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
 
-    const ProgressBar = ({ progress }) => {
-      const [loaderValue, setLoaderValue] = useState(0);
-    
-      useEffect(() => {
-        Animated.timing(loaderValue, {
-          toValue: progress,
-          duration: 500,
-          useNativeDriver: true,
-        }).start();
-      }, [progress]);
-    
-      return (
-        <View style={styles.progressBar}>
-          <Animated.View style={[styles.loader, { width: loaderValue }]} />
-        </View>
-      );
-    };
-    
+  const handleOptionPress = (index) => {
+    if (selectedOption === null) {
+      setSelectedOption(index);
+    }
+  };
 
+  const calculatePercentage = (votes, totalVotes) => {
+    return totalVotes === 0 ? 0 : ((votes / totalVotes) * 100).toFixed(2);
+  };
 
-    const PollListItem =({  poll,
-      onPress = () => {},
-      selectable = false,
-      isSelected = false,})=>{
-        const [numOfVotes, setNumOfVotes] = useState("32")
-        const [userImage, setUserImage] = useState('/Users/sheldonotieno/Squad/assets/person-circle-sharp-pngrepo-com.png')//remember to use uri instead of the require when quering from the backend
-        const [pollCaption, setPollCaption] = useState("dining hall with best food today")
-        const [pollCreator, setPollCreator] = useState("Drake")
-        const [data, setData] = useState([]);
-        
-        const navigation = useNavigation()
-        //fetch for the user's info from the poll being queried
-            //get poll id 
-            useEffect(()=>{
-            const fetchData = async() =>{
-                // //fetch for the poll id -- not sure this is the right way of doing things
-                // //const pollQueryRes = API.graphql(graphqlOperation(getPoll, {id: pollId}))
-                //  //console log the response
-                // //const numOfPollVotes = pollQueryRes.
-                // //fetch user from the poll
-                // const user = pollQueryRes.userID;
-                // const userProfilePic = user.imageUrl;
-                // //update  the photos and the name of the user to be
-                // //console.log(user, userProfile Pic) 
-                // setUserImage(userProfilePic)
-                // const userName = user.name;
-                // setPollCreator(userName)
-                setData(poll.pollItem)
-            }
-             fetchData();
-            }, [])
-            //to do that first get the userID from the poll being queried
-            //then from the user's id let's try and find the user's info 
-            //
-            const renderItem = ({ item }) => {
-              return (
-              <View key={item.id}>
-              <Text>{item.choice}</Text>
-      </View>
-              );
-            };
-
-        return (
-        <Pressable
-          style={styles.container}
-          behavior="padding"
-          >
-         <View
-          style={styles.userImageContainer}
-         >
-         <Image
-         //source={{uri: userImage}}
-         source={require('/Users/sheldonotieno/Squad/assets/person-circle-sharp-pngrepo-com.png')}
-         resizeMode='contain'
-         style={styles.userImage}
-         />
-         </View>
-         
-          <View
-          style = {[styles.pollCaptionContainer]}>
-              <Text
-              style = {styles.pollCaption}>
-                {poll.pollCaption}
-                </Text>     
-           </View>
-    <View style={styles.pollContainer}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        onScroll={(event) => {
-        setProgress(event.nativeEvent.contentOffset.y / 100);
-      }}
-    />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.question}>{poll.pollCaption}</Text>
+      {poll.pollItems.map((option, index) => (
+        <TouchableOpacity
+          key={index}
+          style={[
+            styles.option,
+            selectedOption === index && styles.selectedOption,
+          ]}
+          onPress={() => handleOptionPress(index)}
+        >
+          <Text style={styles.optionText}>{poll.pollItems.pollOption}</Text>
+          {selectedOption !== null && (
+            <StatusBar
+              style={styles.statusBar}
+              translucent
+              backgroundColor="rgba(0, 0, 0, 0.2)"
+              animated
+            >
+              <Text style={styles.statusBarText}>
+                {poll.pollItems.pollOption}: {calculatePercentage(poll.pollItems.votes, poll.totalNumOfVotes)}%
+              </Text>
+            </StatusBar>
+          )}
+        </TouchableOpacity>
+      ))}
+      <Text style={styles.selectedText}>
+        {selectedOption !== null && `You selected: ${poll.pollItems[selectedOption].pollOption}`}
+      </Text>
     </View>
-         
-      </Pressable>
-        )
-        }
-      
-        
-        const styles = StyleSheet.create({
-        container:{
-        flex:1,
-        flexDirection: "row",
-        marginHorizontal: 10,
-        marginTop: 10,
-        marginVertical: 105,
-        borderColor: "#000000",
-        height: 600,
-        width:400,
-        marginLeft:10,
-        borderRadius: 29,
-        backgroundColor: "#D8E8F3",
-        borderWidth: 5
-        // flex:1,
-        //  flexDirection: "row",
-        //  marginHorizontal: 10,
-        //  marginTop: 20,
-        //  //marginVertical: 65,
-        //  borderColor: "#FFFF",
-        //  //height: 100,
-        //  borderRadius: 15,
-        //  backgroundColor: "white",
-        //  borderWidth: 5,
-        //  marginRight:30
-        },
-       pollContainer:{
-        flex: 1,
-        marginTop: 20,
-       },
-        pollCaptionContainer:{
-          height: 50,
-          width: 320,
-          marginTop: 120,
-          flexDirection:'row'
+  );
+};
 
-        },
-        numOfVotesContainer:{
-         height:30,
-         width: 95,
-         backgroundColor: "#1145FD",
-         borderRadius: 40,
-         borderColor: "#C2B960",
-         borderWidth: 2.5,
-         marginLeft:5,
-         
-        },
-        votedText:{
-          color: "white",
-          fontWeight: "bold",
-          marginBottom:7,
-          marginLeft:1,
-          fontSize: 8.5,
-          textAlignVertical:'center'
-        }, 
-        userImageContainer:{
-         marginStart:10,
-         marginTop:10
-        },
-        userImage:{
-            width:50,
-            height:70
-        },
-       pollCaption:{
-        fontWeight:'800',
-        marginRight:15,
-        fontSize:20,
-        marginLeft:-20,
-       },
-       pollCreator:{
-        marginTop: 5,
-        marginLeft: 5,
-        color: '#545454',
-        marginBottom: 450
-       },
-       progressBar: {
-        height: 20,
-        backgroundColor: '#ccc',
-        borderRadius: 4,
-      },
-      loader: {
-        height: 20,
-        backgroundColor: '#000',
-        borderRadius: 4,
-      },
-        
-    },
-        )
-    export default PollListItem;
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  question: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  option: {
+    backgroundColor: '#eee',
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  selectedOption: {
+    backgroundColor: '#3498db', // Change to your selected color
+  },
+  optionText: {
+    fontSize: 16,
+  },
+  selectedText: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  statusBar: {
+    height: 20,
+  },
+  statusBarText: {
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
+
+export default PollListItem;
