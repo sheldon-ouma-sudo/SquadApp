f7b3bde7fc31f9abf385d925cfba37e416cc96fb
sibuor-sudo <otieno1@stanford.edu>
2023-12-26 14:12:15 -0500

    poll feed creation

--- src/components/PollListItem/index.js
+++ src/components/PollListItem/index.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+import {Progress} from 'react-native-progress'
 
 const PollListItem = ({ poll, }) => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  // const [selectedOption, setSelectedOption] = useState(null);
 
-  const handleOptionPress = (index) => {
-    if (selectedOption === null) {
-      setSelectedOption(index);
-    }
-  };
+  // const handleOptionPress = (index) => {
+  //   if (selectedOption === null) {
+  //     setSelectedOption(index);
+  //   }
+  // };
 
-  const calculatePercentage = (votes, totalVotes) => {
-    return totalVotes === 0 ? 0 : ((votes / totalVotes) * 100).toFixed(2);
+  // const calculatePercentage = (votes, totalVotes) => {
+  //   return totalVotes === 0 ? 0 : ((votes / totalVotes) * 100).toFixed(2);
+  // };
+  const renderItem = ({ item }) => {
+    const percentage = (item.votes / poll.totalNumOfVotes) * 100;
+
+    return (
+      <View style={{ marginVertical: 10 }}>
+        <Text>{item.option}</Text>
+        <Progress.Bar progress={percentage / 100} width={200} />
+        <Text>{`${percentage.toFixed(2)}%`}</Text>
+      </View>
+    );
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.question}>{poll.pollCaption}</Text>
-      {poll.pollItems.map((option, index) => (
+      {/* {poll.pollItems.map((option, index) => (
         <TouchableOpacity
           key={index}
           style={[
@@ -43,7 +55,12 @@ const PollListItem = ({ poll, }) => {
       ))}
       <Text style={styles.selectedText}>
         {selectedOption !== null && `You selected: ${poll.pollItems[selectedOption].pollOption}`}
-      </Text>
+      </Text> */}
+       <FlatList
+        data={poll.pollItems}
+        keyExtractor={(item) => item.option}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
